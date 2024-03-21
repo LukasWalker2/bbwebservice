2b5208eae16636142b25fafad64f0f0a5d827e42
LukasWalker2 <lukasogwalker@gmail.com>
2024-03-21 02:07:53 +0100

    new __init__ simplyfied how MAIN_PATH is optained

--- w_bbwebservice/__init__.py
+++ w_bbwebservice/__init__.py
@@ -1,6 +1,6 @@
 #MIT License
 #
-#Copyright (c) [2022] [Lukas Walker]
+#Copyright (c) [2024] Lukas Walker
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -28,7 +28,6 @@ docmentation can be found on <https://bbwebservice.eu>
 
 import os
 import sys
-import re
 
 
 default_config = '''
@@ -42,21 +41,12 @@ default_config = '''
 }
 '''
 
-MAIN_PATH = ''
-modules = sys.modules.copy()
-for key in modules:
-    mod = modules[key]
-    if mod.__name__ == '__main__':
-        MAIN_PATH = "/".join(re.split('/|\\\\',os.path.abspath(mod.__file__))[:-1])
-        
-        if not os.path.exists(MAIN_PATH + "/content"):
-            os.makedirs(MAIN_PATH + "/content")
 
-        if not os.path.exists(MAIN_PATH + "/config"):
-            os.makedirs(MAIN_PATH + "/config")
-
-        if not os.path.exists(MAIN_PATH + "/config/config.json"):
-            with open(MAIN_PATH + "/config/config.json", 'w') as config:
-                config.writelines(str(default_config))
-
-        from .webserver import *
+MAIN_PATH = os.path.dirname(os.path.abspath(sys.argv[0]))
+if not os.path.exists(MAIN_PATH + "/content"):
+    os.makedirs(MAIN_PATH + "/content")
+if not os.path.exists(MAIN_PATH + "/config"):
+    os.makedirs(MAIN_PATH + "/config")
+if not os.path.exists(MAIN_PATH + "/config/config.json"):
+    with open(MAIN_PATH + "/config/config.json", 'w') as config:
+        config.writelines(str(default_config))
